e386617b8cd980415db7e50617d45561e254de3a
maramilod <marammilod03@gmail.com>
2023-11-17 15:04:10 +0000

    uploaded the selection sort function + text answare

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,19 @@
 #include "sort.h"
 /**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ *
+ * @array: array that i need to swap
+ * @size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 	unsigned int index;
 	int temp;
+
+	if (!array || !size)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
