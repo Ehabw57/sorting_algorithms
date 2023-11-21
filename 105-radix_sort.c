78d35c4f9005312dd59f14517e4bd1dc3a01e2ba
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-21 23:07:28 +0200

    fixed a stupid typo
    Replaced '>' by '<'

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -7,42 +7,42 @@
  */
 void count_sort(int *array, size_t size, size_t div)
 {
-    int *count, *copy, i, idx;
+	int *count, *copy, i, idx;
 
-    copy = malloc(sizeof(int) * size);
-    if (!copy)
-        return;
+	copy = malloc(sizeof(int) * size);
+	if (!copy)
+		return;
 
-    count = calloc(10, sizeof(int));
-    if (count == NULL)
-    {
-        free(copy);
-        return;
-    }
-    for (i = 0; i < (int)size; i++)
-        copy[i] = 0;
+	count = calloc(10, sizeof(int));
+	if (count == NULL)
+	{
+		free(copy);
+		return;
+	}
+	for (i = 0; i < (int)size; i++)
+		copy[i] = 0;
 
-    for (i = 0; i < (int)size; i++)
-    {
-        idx = array[i] / div;
-        count[idx % 10]++;
-    }
+	for (i = 0; i < (int)size; i++)
+	{
+		idx = array[i] / div;
+		count[idx % 10]++;
+	}
 
-    for (i = 1; i <= 9; i++)
-        count[i] += count[i - 1];
+	for (i = 1; i <= 9; i++)
+		count[i] += count[i - 1];
 
-    for (i = size - 1; i >= 0; i--)
-    {
-        idx = array[i] / div;
-        copy[count[idx % 10] - 1] = array[i];
-        count[idx % 10]--;
-    }
+	for (i = size - 1; i >= 0; i--)
+	{
+		idx = array[i] / div;
+		copy[count[idx % 10] - 1] = array[i];
+		count[idx % 10]--;
+	}
 
-    for (i = 0; i < (int)size; i++)
-        array[i] = copy[i];
+	for (i = 0; i < (int)size; i++)
+		array[i] = copy[i];
 
-    free(copy);
-    free(count);
+	free(copy);
+	free(count);
 }
 /**
  * radix_sort - Sorts an array of integers using the Radix Sort algorithm.
@@ -51,20 +51,20 @@ void count_sort(int *array, size_t size, size_t div)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max = 0, i;
-    size_t div = 1;
+	int max = 0, i;
+	size_t div = 1;
 
-    if (!array || size > 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (i = 0; i < (int)size; i++)
-        if (array[i] > max)
-            max = array[i];
+	for (i = 0; i < (int)size; i++)
+		if (array[i] > max)
+			max = array[i];
 
-    while (max / div > 0)
-    {
-        count_sort(array, size, div);
-        div *= 10;
-        print_array(array, size);
-    }
+	while (max / div > 0)
+	{
+		count_sort(array, size, div);
+		div *= 10;
+		print_array(array, size);
+	}
 }
